12250d35dfe5475a2fadb8dc3aa56623a06dcf44
ysf1472 <64108009+ysf1472@users.noreply.github.com>
2020-10-23 19:00:09 -0400

    first commit

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,11 +1,44 @@
+// const { Button } = require("bootstrap");
+// const { event } = require("jquery");
+
 // IIFE -- Immediately Invoked Function Expression
-(function(){
+// (function(){
 
-    function start()
-    {
-console.log("App started...");
+//     function Start()
+//     {
+//         console.log("App Started...");
+
+//         let deleteButtons = document.querySelectorAll('.btn-danger');
+        
+//         for(button of deleteButtons)
+//         {
+//             button.addEventListener('click', (event)=>{
+//                 if(!confirm("Are you sure?")) 
+//                 {
+//                     event.preventDefault();
+//                     window.location.assign('/book-list');
+//                 }
+//             });
+//         }
+//     }
+
+//     window.addEventListener("load", Start);
+
+// })();
+(function () {
+    function Start() {
+      console.log("App Started...");
+      let dangerButtons = document.getElementsByClassName("btn-danger");
+      // Adds conformation for deleting buttons
+      for (const button of dangerButtons) {
+        button.addEventListener("click", (event) => {
+          if (!confirm("Are you sure?")) {
+            event.preventDefault();
+            location.href = "/book-list";
+          }
+        });
+      }
     }
-    window.addEventListener("load",start);
-
-})();
-document.getElementById("nextBtn").addEventListener("click", check, false);
+    window.addEventListener("load", Start);
+  })();
+//document.getElementById("nextBtn").addEventListener("click", check, false);
---
+++ server/models/user.js
@@ -0,0 +1,63 @@
+//require modules for the user model
+let mongoose =require(`mongoose`);
+let passportLocalMongoose =require(`passport-local-mongoose`);
+
+
+let User =mongoose.Schema(
+    {
+         username: 
+         {
+                type: String,
+                default: ``,
+                trim: true,
+                required: `username is required`
+         },
+         /*
+         password:
+         {
+                type: String
+                default:``;
+                trim: true,
+                required: `password is required`
+        
+         }
+         */
+        email:
+        {
+            type: String,
+            default: ``,
+            trim: true,
+            required: `email address is required`
+        },
+        displayName:
+        {
+            type: String,
+            default: ``,
+            trim: true,
+            required: `Display Name is required`
+        },
+        created:
+        {
+            type: Date,
+            default: Date.now,
+            
+        },
+        updated:
+        {
+            type: Date,
+            default: Date.now,
+            
+        }
+
+    },
+    {
+            collection: "users"
+    }
+);
+
+// configure options for user model
+
+let options = ({missingPasswordError: ` Wrong / Missing Password`});
+User.plugin(passportLocalMongoose, options);
+
+module.exports.User= mongoose.model(`User`, User);
